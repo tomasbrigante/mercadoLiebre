9486498d93ae5fbbc08e8fc12e6fd40f0c01a437
Tomas Brigante <tomasbrigante19@gmail.com>
2022-03-08 14:30:56 -0300

    primer commit

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3500, ()=>{
-    console.log('Servidor funcionando');
-});
+app.listen(process.env.PORT || 3000, ()=>{ console.log('Servidor funcionando'); });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
